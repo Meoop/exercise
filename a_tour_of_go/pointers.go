a94ab6dcca3847e23f0201d9c096e217cd2978ae
Yilei <yilei108@gmail.com>
2017-12-21 21:28:39 +0800

    go pointer and struct

---
+++ a_tour_of_go/pointers.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	i, j := 42, 2701
+
+	p := &i         // point to i
+	fmt.Println(*p) // read i through the pointer
+	*p = 21         // set i throught the pointer
+	fmt.Println(i)  //set the new value of i
+
+	p = &j         // point to j
+	*p = *p / 37   // divide j through the pointer
+	fmt.Println(j) // see the new value of j
+}
